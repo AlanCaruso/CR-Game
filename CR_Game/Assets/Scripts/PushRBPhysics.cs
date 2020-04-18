73d67c715d3649521479dc5a5611c4f420835279
Alan Caruso <alan.carusoo@gmail.com>
2020-04-18 17:38:30 -0300

    Mass Prototype

--- CR_Game/Assets/Scripts/PushRBPhysics.cs
+++ CR_Game/Assets/Scripts/PushRBPhysics.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PushRBPhysics : MonoBehaviour
 {
     public float pushPower = 2.0f;
+
+    private float targetMass;
+
     public void OnControllerColliderHit(ControllerColliderHit hit)
     {
         Rigidbody body = hit.collider.attachedRigidbody;
@@ -18,8 +21,10 @@ public class PushRBPhysics : MonoBehaviour
 
         }
 
+        targetMass = body.mass;
+
         Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
 
-        body.velocity = pushDir * pushPower;
+        body.velocity = pushDir * pushPower / targetMass;
     }
 }
