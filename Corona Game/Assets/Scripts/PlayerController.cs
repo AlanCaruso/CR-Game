f72e6539f174ba52ff3b15c574108e06fa54247d
Alan Caruso <alan.carusoo@gmail.com>
2020-04-17 23:34:00 -0300

    Relative camera

--- Corona Game/Assets/Scripts/PlayerController.cs
+++ Corona Game/Assets/Scripts/PlayerController.cs
@@ -9,6 +9,12 @@ public class PlayerController : MonoBehaviour
     public CharacterController player;
 
     public float playerSpeed;
+    private Vector3 movePlayer;
+    private Vector3 playerInput;
+
+    public Camera mainCamera;
+    private Vector3 camForward;
+    private Vector3 CamRight;
 
     void Start()
     {
@@ -20,8 +26,25 @@ public class PlayerController : MonoBehaviour
     {
         horizontalMove = Input.GetAxis("Horizontal");
         verticalMove = Input.GetAxis("Vertical");
+
+        player.Move(movePlayer * playerSpeed * Time.deltaTime);
+        playerInput = new Vector3(horizontalMove, 0, verticalMove);
+        playerInput = Vector3.ClampMagnitude(playerInput, 1);
+        camDirection();
+        Debug.Log(player.velocity.magnitude);
+        movePlayer = playerInput.x * CamRight + playerInput.z * camForward;
+
+        player.transform.LookAt(player.transform.position + movePlayer);
     }
-    void FixedUpdate(){
-        player.Move(new Vector3(horizontalMove, 0, verticalMove) * playerSpeed * Time.deltaTime);
+    
+    void camDirection(){
+        camForward = mainCamera.transform.forward;
+        CamRight = mainCamera.transform.right;
+
+        camForward.y = 0;
+        CamRight.y = 0;
+
+        camForward = camForward.normalized;
+        CamRight = CamRight.normalized;
     }
 }
